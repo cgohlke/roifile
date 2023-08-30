3fa7f930dbb0877e34325b1e11f5412d5b4da0a7
Christoph Gohlke <cgohlke@cgohlke.com>
2023-08-29 20:54:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 """Roifile package setuptools script."""
 
-import sys
 import re
+import sys
 
 from setuptools import setup
 
@@ -16,10 +16,10 @@ def search(pattern, code, flags=0):
     return match.groups()[0]
 
 
-with open('roifile/roifile.py') as fh:
+with open('roifile/roifile.py', encoding='utf-8') as fh:
     code = fh.read()
 
-version = search(r"__version__ = '(.*?)'", code)
+version = search(r"__version__ = '(.*?)'", code).replace('.x.x', '.dev0')
 
 description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
 
@@ -42,10 +42,10 @@ license = search(
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
-    with open('LICENSE', 'w') as fh:
+    with open('LICENSE', 'w', encoding='utf-8') as fh:
         fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
-    with open('README.rst', 'w') as fh:
+    with open('README.rst', 'w', encoding='utf-8') as fh:
         fh.write(readme)
 
 setup(
@@ -54,6 +54,7 @@ setup(
     license='BSD',
     description=description,
     long_description=readme,
+    long_description_content_type='text/x-rst',
     author='Christoph Gohlke',
     author_email='cgohlke@cgohlke.com',
     url='https://www.cgohlke.com',
@@ -63,8 +64,9 @@ setup(
         # 'Documentation': 'https://',
     },
     packages=['roifile'],
+    package_data={'roifile': ['py.typed']},
     entry_points={'console_scripts': ['roifile = roifile.roifile:main']},
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=['numpy'],
     extras_require={'all': ['matplotlib', 'tifffile']},
     platforms=['any'],
@@ -78,5 +80,6 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
